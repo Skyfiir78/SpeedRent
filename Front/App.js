e43ff4fb2e56afcd4125c5b5b9b94e46dfad30e2
steven <steven.sammarone@epitech.eu>
2019-10-05 16:42:18 +0200

    architecture in dev fonctional for login

--- Front/App.js
+++ Front/App.js
@@ -1,19 +1,11 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import AuthStack from './Component/Stack/Auth'
+
+export default createAppContainer(createSwitchNavigator({
+    Auth: AuthStack
+},
+{
+    initialRouteName: 'Auth'
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+))
